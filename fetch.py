810e2a8e5b0ece586cbb299ecf0198635f8f9a73
Diego Giorgini <diego.giorgini@icloud.com>
2025-03-14 23:17:38 +0100

    Initial commit: fetch.py to save data to db; print_db.py to print all data

---
+++ fetch.py
@@ -0,0 +1,74 @@
+from datetime import datetime, timezone
+import os
+import sqlite3
+from zoneinfo import ZoneInfo
+
+import aranet4
+
+
+def main():
+    num_retries = 10
+    device_name = "camera"
+    device_mac = '11A2FFE6-EC4D-D53D-9695-EA19DCE33F63'
+
+    db_path = os.path.join(os.path.expanduser('~'), 'Documents/aranet4.db')
+    entry_filter = {}
+
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS measurements(
+            device TEXT,
+            timestamp INTEGER,
+            temperature REAL,
+            humidity INTEGER,
+            pressure REAL,
+            CO2 INTEGER,
+            PRIMARY KEY(device, timestamp)
+        )
+    '''
+    )
+    con.commit()
+
+    res = cur.execute('''SELECT timestamp FROM measurements WHERE device = ?
+                         ORDER BY timestamp DESC LIMIT 1''', (device_name,))
+    row = res.fetchone()
+    if row is not None:
+        entry_filter['start'] = datetime.fromtimestamp(row[0], tz=timezone.utc).astimezone(ZoneInfo("Europe/Rome"))
+    history = None
+    for attempt in range(num_retries):
+        entry_filter['end'] = datetime.now(timezone.utc).astimezone(ZoneInfo("Europe/Rome"))
+        try:
+            history = aranet4.client.get_all_records(device_mac, entry_filter)
+            break
+        except Exception as e:
+            print('attempt', attempt, 'failed, retrying:', e)
+
+    data = []
+    if history is not None:
+        for entry in history.value:
+            if entry.co2 < 0:
+                continue
+
+            data.append((
+                device_name,
+                entry.date.timestamp(),
+                entry.temperature,
+                entry.humidity,
+                entry.pressure,
+                entry.co2
+            ))
+        print('fetched', len(data), 'measurements', entry_filter)
+        cur.executemany(
+            'INSERT OR IGNORE INTO measurements VALUES(?, ?, ?, ?, ?, ?)', data
+        )
+        con.commit()
+    else:
+        print('failed to fetch measurements')
+
+    con.close()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ print_db.py
@@ -0,0 +1,42 @@
+import os
+import datetime
+import sqlite3
+from zoneinfo import ZoneInfo
+
+def format_time(timestamp):
+    dt = datetime.datetime.fromtimestamp(timestamp, tz=datetime.timezone.utc)
+    local_dt = dt.astimezone(ZoneInfo("Europe/Rome"))
+    return local_dt.strftime('%Y-%m-%d %H:%M:%S %z')
+
+
+def main():
+    db_path = os.path.join(os.path.expanduser('~'), 'aranet4.db')
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+    _ = cur.execute(
+        '''
+        SELECT device, timestamp, temperature, humidity, pressure, CO2 FROM measurements
+        ORDER BY timestamp
+        '''
+    )
+
+    column_names = [description[0] for description in cur.description]
+    print(' | '.join(column_names))
+    print('-' * (sum(len(name) for name in column_names) + 3 * (len(column_names) - 1)))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        row = list(row)
+        row[1] = format_time(row[1])
+        print(' | '.join(str(value) for value in row))
+
+    print(f"Number of measurements: {len(rows)}")
+
+    con.close()
+
+
+if __name__ == "__main__":
+    main()
+
+
